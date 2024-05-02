4ee5085160da17987dcfea7012db6e3f63690b68
Griffin Stofko <gpstofko@cps.edu>
2024-05-02 11:21:21 -0500

    FINAL WORKING 5/2/2024

--- neural_net_obesity_data.py
+++ neural_net_obesity_data.py
@@ -13,51 +13,50 @@ def parse_line(line: str) -> Tuple[List[float], List[float]]:
     """
     tokens = line.split(",")
     out = (tokens[16])
-    print(out)
-    output = [0 if out == "Normal_Weight" else 0.33 if out == 'Obesity_Type_I'  else 0.66 if out == "Obesity_Type_II" else 1]
-
+    output = [0 if out == "Insufficient_Weight" else 0.166 if out == '0rmal_Weight'  else 0.333 if out == "Overweight_Level_I" else 0.5 if out == "Overweight_Level_II" else 0.666 if out == "Obesity_Type_I" else 0.833 if out == "Obesity_Type_II" else 1]
+#Insufficient_Weight, 0rmal_Weight, Overweight_Level_I, Overweight_Level_II, Obesity_Type_I, Obesity_Type_II, Obesity_Type_III
     inpt = [float(x) for x in tokens[:16]]
     return (inpt, output)
 
 
-# def normalize(data: List[Tuple[List[float], List[float]]]):
-#     """Makes the data range for each input feature from 0 to 1
+def normalize(data: List[Tuple[List[float], List[float]]]):
+    """Makes the data range for each input feature from 0 to 1
 
-#     Args:
-#         data - list of (input, output) tuples
+    Args:
+        data - list of (input, output) tuples
 
-#     Returns:
-#         normalized data where input features are mapped to 0-1 range (output already
-#         mapped in parse_line)
-#     """
-#     leasts = len(data[0][0]) * [100.0]
-#     mosts = len(data[0][0]) * [0.0]
+    Returns:
+        normalized data where input features are mapped to 0-1 range (output already
+        mapped in parse_line)
+    """
+    leasts = len(data[0][0]) * [100.0]
+    mosts = len(data[0][0]) * [0.0]
 
-#     for i in range(len(data)):
-#         for j in range(len(data[i][0])):
-#             if data[i][0][j] < leasts[j]:
-#                 leasts[j] = data[i][0][j]
-#             if data[i][0][j] > mosts[j]:
-#                 mosts[j] = data[i][0][j]
+    for i in range(len(data)):
+        for j in range(len(data[i][0])):
+            if data[i][0][j] < leasts[j]:
+                leasts[j] = data[i][0][j]
+            if data[i][0][j] > mosts[j]:
+                mosts[j] = data[i][0][j]
 
-#     for i in range(len(data)):
-#         for j in range(len(data[i][0])):
-#             data[i][0][j] = (data[i][0][j] - leasts[j]) / (mosts[j] - leasts[j])
-#     return data
+    for i in range(len(data)):
+        for j in range(len(data[i][0])):
+            data[i][0][j] = (data[i][0][j] - leasts[j]) / (mosts[j] - leasts[j])
+    return data
 
 
 with open("ObesityDataSet_raw_and_data_sinthetic.csv", "r") as f:
     training_data = [parse_line(line) for line in f.readlines() if len(line) > 4]
 
-print(training_data)
-# td = normalize(training_data)
+# print(training_data)
+td = normalize(training_data)
 # print(td)
 
-# train, test = train_test_split(td)
+train, test = train_test_split(td)
 
-# nn = NeuralNet(13, 3, 1)
-# nn.train(train, iters=10000, print_interval=1000, learning_rate=0.2)
+nn = NeuralNet(16, 3, 1)
+nn.train(train, iters=1000, print_interval=100, learning_rate=0.2)
 
-# for i in nn.test_with_expected(test):
-#     difference = round(abs(i[1][0] - i[2][0]), 3)
-#     print(f"desired: {i[1]}, actual: {i[2]} diff: {difference}")
+for i in nn.test_with_expected(test):
+    difference = round(abs(i[1][0] - i[2][0]), 3)
+    print(f"desired: {i[1]}, actual: {i[2]} diff: {difference}")
